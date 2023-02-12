3b520c094b961d94e350f12d9cc0b716fe416913
harishsambasivam <imharishsambasivam@gmail.com>
2023-02-12 23:25:02 +0530

    feat: wired forms with backend and added redirects based on auth context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import SignUp from "./Pages/Signup";
 import SignIn from "./Pages/Signin";
+import AuthContext from "./context/AuthContext";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
-export const UserContext = createContext({});
+function getInitialAuthState(refreshToken: string | null) {
+  return refreshToken ? true : false;
+}
 
 function App() {
+  const [refreshToken] = useLocalStorage("refreshToken", null);
+  const [authenticated, setAuthenticated] = useState(
+    getInitialAuthState(refreshToken) || false
+  );
+
   return (
-    <UserContext.Provider value="">
+    <AuthContext.Provider value={{ authenticated, setAuthenticated }}>
       <div className="container w-screen h-screen mx-auto p-4 flex flex-col justify-center items-center">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -16,7 +25,7 @@ function App() {
           <Route path="signup" element={<SignUp />}></Route>
         </Routes>
       </div>
-    </UserContext.Provider>
+    </AuthContext.Provider>
   );
 }
 
